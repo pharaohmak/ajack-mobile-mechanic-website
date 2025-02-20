c5ee4340b5484ecaccdf8fad2f9690c8c8722c9d
Makram M. Ibrahim <61184060+pharaohmak@users.noreply.github.com>
2025-02-20 03:43:02 -0700

    updates to styling

--- scripts.js
+++ scripts.js
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const navToggle = document.querySelector('.main-nav__toggle');
     const navList = document.querySelector('.main-nav__list');
+    const overlay = document.getElementById('overlay');
 
     navToggle.addEventListener('click', function () {
         const expanded = navToggle.getAttribute('aria-expanded') === 'true' || false;
         navToggle.setAttribute('aria-expanded', !expanded);
         navList.classList.toggle('main-nav__list--open');
+        overlay.classList.toggle('active');
+    });
+
+    overlay.addEventListener('click', function () {
+        navToggle.setAttribute('aria-expanded', false);
+        navList.classList.remove('main-nav__list--open');
+        overlay.classList.remove('active');
     });
 });
